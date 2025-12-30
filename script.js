a97809799212691ba1a18e49c15a7a095a1015a7
Mukundhan P <mukundanuma07@gmail.com>
2025-12-30 22:23:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var sidenav=document.querySelector(".sidenav-bar")
+sidenav.style.display ="none"
+
+function showNavbar(){
+    sidenav.style.display="block"
+}
+
+
+function closeNavbar(){
+    sidenav.style.display="none"
+}
